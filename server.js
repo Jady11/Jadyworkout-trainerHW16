0cd8d66e1b495fbb04102b5da612050d2e96064a
Jady Tippetts <jady11@gmail.com>
2021-01-21 12:20:33 -0700

    deploy heroko and atlas

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  
 });
 
 // routes
